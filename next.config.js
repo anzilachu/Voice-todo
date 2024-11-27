77ae472c7b08234d28e240a3ad4e4f7c4f4c6771
Anzil K <91861809+anzilachu@users.noreply.github.com>
2024-11-27 10:19:46 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -25,8 +25,14 @@ const nextConfig = {
           },
         ],
       },
-    ]
+    ];
   },
-}
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'https://voice-pro.vercel.app',
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || '523654605979-frv1vh91r720e3flmsodetcq2jacu6v4.apps.googleusercontent.com',
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || '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',
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || 'bvNYQvStBw14BehY4smf4YzfU3/0azHfibn44U9CzVA=',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
